200631fd9f0604bf6b9ec2f5112c8e0b3e8afb90
dev.liang <dev.liang@outlook.com>
2019-04-13 23:45:15 +0800

    add test Create 操作符

--- app/src/main/java/com/android/rxjavaproject/MainActivity.java
+++ app/src/main/java/com/android/rxjavaproject/MainActivity.java
@@ -1,13 +1,101 @@
 package com.android.rxjavaproject;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.reactivestreams.Subscriber;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
+    StringBuffer mRxOperatorsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testCreat();
     }
+
+    /**
+     * 需要注意的几点是：
+     * <p>
+     * 在发射事件中，我们在发射了数值 3 之后，直接调用了 e.onComlete()，虽然无法接收事件，但发送事件还是继续的。
+     * 另外一个值得注意的点是，在 RxJava 2.x 中，可以看到发射事件方法相比 1.x 多了一个 throws Excetion，意味着我们做一些特定操作再也不用 try-catch 了。
+     * 并且 2.x 中有一个 Disposable 概念，这个东西可以直接调用切断，可以看到，当它的 isDisposed() 返回为 false 的时候，接收器能正常接收事件，但当其为 true 的时候，接收器停止了接收。所以可以通过此参数动态控制接收事件了
+     */
+    private void testCreat() {
+        mRxOperatorsText = new StringBuffer();
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                // 发送事件
+                mRxOperatorsText.append("Observable emit 1" + "\n");
+                Log.e(TAG, "Observable emit 1" + "\n\n");
+                e.onNext(1);
+//                e.onComplete();
+
+                mRxOperatorsText.append("Observable emit 2" + "\n");
+                Log.e(TAG, "Observable emit 2" + "\n\n");
+                e.onNext(2);
+
+
+                mRxOperatorsText.append("Observable emit 3" + "\n");
+                Log.e(TAG, "Observable emit 3" + "\n\n");
+                e.onNext(3);
+                e.onComplete();
+
+                mRxOperatorsText.append("Observable emit 4" + "\n");
+                Log.e(TAG, "Observable emit 4" + "\n\n");
+                e.onNext(4);
+            }
+        }).subscribe(new Observer<Integer>() {
+            private int i;
+            private Disposable mDisposable;
+
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                mRxOperatorsText.append("onSubscribe : " + d.isDisposed() + "\n");
+                Log.e(TAG, "onSubscribe : " + d.isDisposed() + "\n");
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                // 接收事件
+                mRxOperatorsText.append("onNext : value : " + integer + "\n");
+                Log.e(TAG, "onNext : value : " + integer + "\n");
+                i++;
+                if (i == 2) {
+                    // 在RxJava 2.x 中，新增的Disposable可以做到切断的操作，让Observer观察者不再接收上游事件
+                    mDisposable.dispose();
+                    mRxOperatorsText.append("onNext : isDisposable : " + mDisposable.isDisposed() + "\n");
+                    Log.e(TAG, "onNext : isDisposable : " + mDisposable.isDisposed() + "\n");
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                mRxOperatorsText.append("onError : value : " + e.getMessage() + "\n");
+                Log.e(TAG, "onError : value : " + e.getMessage() + "\n");
+            }
+
+            @Override
+            public void onComplete() {
+                mRxOperatorsText.append("onComplete" + "\n");
+                Log.e(TAG, "onComplete" + "\n");
+            }
+        });
+
+    }
+
+
 }
