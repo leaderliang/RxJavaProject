5c70d3c0547b84c986301105669c8bf8823858c5
dev.liang <dev.liang@outlook.com>
2019-04-15 01:10:58 +0800

    add test method create、map、zip、Concat、flatMap、concatMap 操作符

--- app/src/main/java/com/android/rxjavaproject/MainActivity.java
+++ app/src/main/java/com/android/rxjavaproject/MainActivity.java
@@ -7,12 +7,32 @@ import android.util.Log;
 
 import org.reactivestreams.Subscriber;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
+/**
+ *
+ * 各个操作符执行的结果通过 log 日志来进行查看
+ * 操作符
+ * create、map、zip、Concat、flatMap、concatMap
+ *
+ *
+ * @author liang
+ */
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = getClass().getSimpleName();
@@ -22,8 +42,194 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testCreat();
+        mRxOperatorsText = new StringBuffer();
+//        testCreate();
+//        testMap();
+//        testZip();
+//        testConcat();
+
+        testFlatMap();
+//        testConcatMap();
     }
+
+    /**
+     * concatMap 与 FlatMap 的唯一区别就是 concatMap 保证了顺序
+     */
+    private void testConcatMap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+            }
+        }).concatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(@NonNull Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime, TimeUnit.MILLISECONDS);
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+                        Log.e(TAG, "flatMap : accept : " + s + "\n");
+                        mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                    }
+                });
+    }
+
+    /**
+     * 它可以把一个发射器 Observable 通过某种方法转换为多个 Observables，
+     * 然后再把这些分散的 Observables装进一个单一的发射器 Observable。
+     * 但有个需要注意的是，flatMap 并不能保证事件的顺序，如果需要保证，需要用到我们下面要讲的 ConcatMap。
+     *
+     */
+    private void testFlatMap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime,TimeUnit.SECONDS);
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                Log.e(TAG, "flatMap : accept : " + s + "\n");
+            }
+        });
+
+    }
+
+    /**
+     * 把两个发射器连接成一个发射器
+     */
+    private void testConcat() {
+        Observable.concat(Observable.just("4","4","6"), Observable.just("5","5","7")).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                mRxOperatorsText.append(s);
+                Log.e(TAG, "concat : accept : " + mRxOperatorsText.toString() + "\n");
+            }
+        });
+    }
+
+    /**
+     * zip 组合事件的过程就是分别从发射器 A 和发射器 B 各取出一个事件来组合，并且一个事件只能被使用一次，
+     * 组合的顺序是严格按照事件发送的顺序来进行的，所以上面截图中，可以看到，1 永远是和 A 结合的，2 永远是和 B 结合的。
+     *
+     * 最终接收器收到的事件数量是和发送器发送事件最少的那个发送器的发送事件数目相同, getStringObservable()发送器发的事件数目最少.
+     *
+     */
+    private void testZip() {
+        Observable.zip(getIntegerObservable(), getStringObservable(), new BiFunction<Integer,String, String>() {
+            @Override
+            public String apply(@NonNull Integer integer,@NonNull String s) throws Exception {
+                Log.e(TAG, "s + integer : " + s + integer + "\n");
+                return s + integer;
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                mRxOperatorsText.append("zip : accept : " + s + "\n");
+                Log.e(TAG, "zip : accept : " + s + "\n");
+            }
+        });
+    }
+
+    private Observable<String> getStringObservable() {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext("A");
+                    mRxOperatorsText.append("String emit : A \n");
+                    Log.e(TAG, "second request--> String emit : A \n");
+                    e.onNext("B");
+                    mRxOperatorsText.append("String emit : B \n");
+                    Log.e(TAG, "second request--> String emit : B \n");
+                    e.onNext("C");
+                    mRxOperatorsText.append("String emit : C \n");
+                    Log.e(TAG, "second request--> String emit : C \n");
+                }
+            }
+        });
+    }
+
+    private Observable<Integer> getIntegerObservable() {
+        return Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                if (!e.isDisposed()) {
+                    e.onNext(1);
+                    mRxOperatorsText.append("Integer emit : 1 \n");
+                    Log.e(TAG, "first request--> Integer emit : 1 \n");
+                    e.onNext(2);
+                    mRxOperatorsText.append("Integer emit : 2 \n");
+                    Log.e(TAG, "first request--> Integer emit : 2 \n");
+                    e.onNext(3);
+                    mRxOperatorsText.append("Integer emit : 3 \n");
+                    Log.e(TAG, "first request--> Integer emit : 3 \n");
+                    e.onNext(4);
+                    mRxOperatorsText.append("Integer emit : 4 \n");
+                    Log.e(TAG, "first request--> Integer emit : 4 \n");
+                    e.onNext(5);
+                    mRxOperatorsText.append("Integer emit : 5 \n");
+                    Log.e(TAG, "first request--> Integer emit : 5 \n");
+                }
+            }
+        });
+    }
+
+    /**
+     * Map 基本算是 RxJava 中一个最简单的操作符了,它的作用是对发射时间发送的每一个事件应用一个函数，
+     * 是的每一个事件都按照指定的函数去变化。
+     *
+     * map 基本作用就是将一个 Observable 通过某种函数关系，转换为另一种 Observable，
+     * 例子中就是把我们的 String 数据变成了 float 类型，查看 Log 日志
+     */
+    private void testMap() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("234");
+                emitter.onNext("678");
+                emitter.onNext("456");
+            }
+        }).map(new Function<String, Float>() {
+            @Override
+            public Float apply(String string) throws Exception {
+                return Float.valueOf(string);
+            }
+        }).subscribe(new Consumer<Float>() {
+            @Override
+            public void accept(Float aFloat) throws Exception {
+                mRxOperatorsText.append("accept : " + aFloat +"\n");
+                Log.e(TAG, "accept : " + aFloat +"\n" );
+            }
+        });
+    }
+
+
 
     /**
      * 需要注意的几点是：
@@ -32,8 +238,8 @@ public class MainActivity extends AppCompatActivity {
      * 另外一个值得注意的点是，在 RxJava 2.x 中，可以看到发射事件方法相比 1.x 多了一个 throws Excetion，意味着我们做一些特定操作再也不用 try-catch 了。
      * 并且 2.x 中有一个 Disposable 概念，这个东西可以直接调用切断，可以看到，当它的 isDisposed() 返回为 false 的时候，接收器能正常接收事件，但当其为 true 的时候，接收器停止了接收。所以可以通过此参数动态控制接收事件了
      */
-    private void testCreat() {
-        mRxOperatorsText = new StringBuffer();
+    private void testCreate() {
+
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
@@ -96,6 +302,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+
 
 
 }
